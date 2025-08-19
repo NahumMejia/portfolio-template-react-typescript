c55b417b20fd582890fa2a12e77d115c37e38291
Nahum <nahummejiad@gmail.com>
2025-08-19 17:44:44 -0600

    Minor changes

--- src/Data/Experience.ts
+++ src/Data/Experience.ts
@@ -5,9 +5,9 @@ export const experience = [
         icon: "material-symbols-light:computer-sharp",
         description: "Worked as a backend developer for Todo Go, a service app similar to Uber Eats but with various service types. Designed and maintained APIs to support different types of service categories and user interactions. Focused on ensuring scalability, security, and performance of the backend infrastructure.",
         responsibilities: [
-            "Designed RESTful APIs to handle various service types, user orders, and real-time tracking.",
-            "Collaborated with frontend developers to ensure smooth integration between backend services and user interfaces.",
-            "Participated in the full software development lifecycle, including requirements gathering, design, development, testing, and deployment.",
+        "Acted as a functional analyst, translating business requirements into technical specifications and ensuring alignment between stakeholders and development teams.",
+        "Led backend development efforts, architecting and implementing scalable RESTful APIs to support service management, user orders, and real-time tracking.",
+        "Contributed across the full software development lifecycle, from gathering requirements and designing solutions to coding.",
         ],
     },
     {
--- src/Data/Technologies.ts
+++ src/Data/Technologies.ts
@@ -4,8 +4,6 @@ export const technologies = [
     { name: "React", icon: "cib:react", color: "#61DBFB" },
     { name: "Tailwind CSS", icon: "bxl:tailwind-css", color: "#38BDF8" },
     { name: "MySQL", icon: "grommet-icons:mysql", color: "#00758F" },
-    { name: "Docker", icon: "simple-icons:docker", color: "#2496ED" },
-    { name: "MongoDB", icon: "simple-icons:mongodb", color: "#47A248" },
     { name: "JavaScript", icon: "akar-icons:javascript-fill", color: "#F7DF1E" },
     { name: "TypeScript", icon: "cib:typescript", color: "#3178C6" },
     { name: "Git", icon: "simple-icons:git", color: "#F05032" },
